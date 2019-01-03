0a2a67e0a46330065db114507c4460f9b06fb827
gerhard6380 <gerhard@seeber.at>
2019-01-03 23:12:15 +0100

    create new projects for iOS and macOS, some basic classes in PCL to launch the new projects with a sample page

---
+++ Friendica Mobile.macOS/Helpers/FontIconLabelRenderer.cs
@@ -0,0 +1,29 @@
+﻿using AppKit;
+using Friendica_Mobile.macOS.Helpers;
+using Friendica_Mobile.PCL.Styles;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+[assembly: ExportRenderer(typeof(FontIconLabel), typeof(FontIconLabelRenderer))]
+namespace Friendica_Mobile.macOS.Helpers
+{
+    
+    public class FontIconLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                
+                var label = new NSTextField();
+                label.StringValue = e.NewElement.Text;
+                label.Bordered = false;
+                label.DrawsBackground = false;
+                label.TextColor = e.NewElement.TextColor.ToNSColor();
+                label.Font = NSFont.FromFontName("Segoe MDL2 Assets", 20);
+                SetNativeControl(label);
+            }
+        }
+    }
+}
---
+++ Friendica Mobile.macOS/Helpers/GetSystemTheme.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Friendica_Mobile.PCL;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Friendica_Mobile.macOS.GetSystemTheme))]
+namespace Friendica_Mobile.macOS
+{
+    public class GetSystemTheme : IGetSystemTheme
+    {
+        public App.ApplicationTheme GetSelectedTheme()
+        {
+            // depending on the system we will try to get the system default theme (UWP, macos?)
+            // we do not have a dark theme on ios at the moment (NOV-2018)
+
+            // secondly we will check the user settings, as user can define a preferred theme within the app
+            if (!Settings.AppThemeUseSystemTheme)
+                return (Settings.AppThemeDarkModeEnabled) ? App.ApplicationTheme.Dark : App.ApplicationTheme.Light;
+
+            // as a fallback the system will define Light theme as a default value
+            return App.ApplicationTheme.Light;
+        }
+    }
+}
