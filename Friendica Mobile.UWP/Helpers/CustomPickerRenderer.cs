308d9bb44be0aed11a38a1be0722dd33a5534725
gerhard6380 <gerhard@seeber.at>
2019-02-01 00:59:38 +0100

    implement adjustments for Picker control

---
+++ Friendica Mobile.UWP/Helpers/CustomPickerRenderer.cs
@@ -0,0 +1,47 @@
+﻿using Friendica_Mobile.UWP;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(Picker), typeof(CustomPickerRenderer))]
+namespace Friendica_Mobile.UWP
+{
+    public class CustomPickerRenderer : PickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+            if (this.Control != null)
+            {
+                e.NewElement.PropertyChanged += NewElement_PropertyChanged;
+                SetStyle(e.NewElement as Picker);
+            }
+        }
+
+        private void NewElement_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (Control != null)
+                SetStyle(sender as Picker);
+        }
+
+        void SetStyle(Picker picker)
+        {
+            // remove text header because we will need to place a separate header label for all other platforms
+            if (Control != null)
+                Control.Header = "";
+
+            // needed to avoid light style when temporary have crazy values with all -1
+            if (picker.BackgroundColor.A == -1)
+                return; 
+
+            // set theme as per setting in the app
+            if (Control != null)
+            {
+                if (Friendica_Mobile.App.SelectedTheme == Friendica_Mobile.App.ApplicationTheme.Light)
+                    Control.RequestedTheme = Windows.UI.Xaml.ElementTheme.Light;
+                else
+                    Control.RequestedTheme = Windows.UI.Xaml.ElementTheme.Dark;
+            }
+        }
+    }
+}
---
+++ Friendica Mobile.macOS/Helpers/CustomPickerRenderer.cs
@@ -0,0 +1,52 @@
+﻿using AppKit;
+using Friendica_Mobile.macOS;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+[assembly: ExportRenderer(typeof(Picker), typeof(CustomPickerRenderer))]
+namespace Friendica_Mobile.macOS
+{
+
+    public class CustomPickerRenderer : PickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || this.Element == null)
+                return;
+
+            e.NewElement.PropertyChanged += NewElement_PropertyChanged;
+            SetColors(e.NewElement);
+        }
+
+        void NewElement_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "IsEnabled")
+            {
+                var picker = sender as Picker;
+                SetColors(picker); 
+            }
+        }
+
+
+        void SetColors(Picker picker)
+        {
+            // override with black text color if we have a light theme background because user could have probably
+            // dark system theme leading to white as primary text color vanishing the inputs
+            // disabled input automatically makes black text visible in gray
+            if (picker.BackgroundColor == Color.White || picker.BackgroundColor == Color.WhiteSmoke)
+            {
+                Control.TextColor = NSColor.Black;
+                Control.BackgroundColor = (picker.IsEnabled) ? NSColor.White : NSColor.FromRgb(245, 245, 245);;
+                //Control.Bordered = true;
+            }
+            else
+            {
+                //Control.Bordered = false;
+                Control.BackgroundColor = (picker.IsEnabled) ? NSColor.FromRgb(153, 153, 153) : NSColor.FromRgb(51, 51, 51);
+            }
+        }
+    }
+}
