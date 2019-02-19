3a09aad3cc03b1eadfce1eb36caa6e301c14e5a4
gerhard6380 <gerhard@seeber.at>
2019-02-19 22:35:41 +0100

    Help page in Xamarin Forms

---
+++ Friendica Mobile/ViewModel/HelpViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System.Windows.Input;
+using Friendica_Mobile.Strings;
+using Xamarin.Forms;
+using System;
+
+namespace Friendica_Mobile.ViewModel
+{
+    public class HelpViewModel : BaseViewModel
+    {
+
+
+#region Commands
+
+        private ICommand _linkAppSupportpageCommand;
+        public ICommand  LinkAppSupportpageCommand => _linkAppSupportpageCommand ?? (_linkAppSupportpageCommand = new Command(LinkAppSupportpage));
+        private void LinkAppSupportpage()
+        {
+            string baseUrl = "https://friendica.hasecom.at/profile/friendicamobile{0}";
+            var profile = "";
+            if (Settings.IsFriendicaLoginDefined())
+            {
+                // show support page with zrl link of user
+                profile = "?zrl=" + Uri.EscapeUriString(Settings.FriendicaServer) + "/profile/" + Uri.EscapeUriString(Settings.FriendicaUsername);
+            }
+            var url = new Uri(String.Format(baseUrl, profile));
+            Device.OpenUri(url);
+        }
+
+        private ICommand _linkFriendicaSupportCommand;
+        public ICommand LinkFriendicaSupportCommand => _linkFriendicaSupportCommand ?? (_linkFriendicaSupportCommand = new Command(LinkFriendicaSupport));
+        private void LinkFriendicaSupport()
+        {
+            string baseUrl = "https://forum.friendi.ca/profile/helpers{0}";
+            var profile = "";
+            if (Settings.IsFriendicaLoginDefined())
+            {
+                // show support page with zrl link of user
+                profile = "?zrl=" + Uri.EscapeUriString(Settings.FriendicaServer) + "/profile/" + Uri.EscapeUriString(Settings.FriendicaUsername);
+            }
+            var url = new Uri(String.Format(baseUrl, profile));
+            Device.OpenUri(url);
+        }
+
+        private ICommand _linkFriendicaDevelopersCommand;
+        public ICommand LinkFriendicaDevelopersCommand => _linkFriendicaDevelopersCommand ?? (_linkFriendicaDevelopersCommand = new Command(LinkFriendicaDevelopers));
+        private void LinkFriendicaDevelopers()
+        {
+            string baseUrl = "https://forum.friendi.ca/profile/developers{0}";
+            var profile = "";
+            if (Settings.IsFriendicaLoginDefined())
+            {
+                // show support page with zrl link of user
+                profile = "?zrl=" + Uri.EscapeUriString(Settings.FriendicaServer) + "/profile/" + Uri.EscapeUriString(Settings.FriendicaUsername);
+            }
+            var url = new Uri(String.Format(baseUrl, profile));
+            Device.OpenUri(url);
+        }
+
+        private ICommand _hyperlinkGithubCommand;
+        public ICommand HyperlinkGithubCommand => _hyperlinkGithubCommand ?? (_hyperlinkGithubCommand = new Command(HyperlinkGithub));
+        public void HyperlinkGithub()
+        {
+            var url = new Uri("https://github.com/friendica/friendica");
+            Device.OpenUri(url);
+        }
+
+        private ICommand _hyperlinkFriendicaCommand;
+        public ICommand HyperlinkFriendicaCommand => _hyperlinkFriendicaCommand ?? (_hyperlinkFriendicaCommand = new Command(HyperlinkFriendica));
+        public void HyperlinkFriendica()
+        {
+            var url = new Uri("https://friendi.ca");
+            Device.OpenUri(url);
+        }
+
+        // https://www.youtube.com/channel/UCnRt5h9VTtaec3QAn6u3mqQ
+
+#endregion
+
+
+        public HelpViewModel()
+        {
+            Title = AppResources.pageTitleHelp_Text;
+        }
+
+
+
+    }
+}
--- Friendica Mobile/Views/92_Help.xaml.cs
+++ Friendica Mobile/Views/92_Help.xaml.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
+using Friendica_Mobile.Styles;
+using Friendica_Mobile.ViewModel;
 using Xamarin.Forms;
 
 namespace Friendica_Mobile.Views
 {
-    public partial class Help : ContentPage
+    public partial class Help : BaseContentPage
     {
         public Help()
         {
             InitializeComponent();
+
+            // TODO: funktioniert noch nicht zuverlässig
+            //var mvvm = this.BindingContext as HelpViewModel;
+            //var click = new ClickGestureRecognizer();
+            //click.Clicked += (sender, e) => 
+            //{
+            //    mvvm.HyperlinkGithub();
+            //};
+            //SpanGithubLink.GestureRecognizers.Add(click);
+
+            //var clickFriendica = new ClickGestureRecognizer();
+            //clickFriendica.Clicked += (sender, e) => 
+            //{
+            //    mvvm.HyperlinkFriendica();
+            //};
+            //SpanFriendicaLink.GestureRecognizers.Add(clickFriendica);
         }
     }
 }
